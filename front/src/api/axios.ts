1dae59cb17c0ae73aeeda075daffb765ee895048
Serhii Slavita <serhii.slavita@gmail.com>
2021-01-24 21:34:54 +0200

    refactoring

--- front/src/api/axios.ts
+++ front/src/api/axios.ts
@@ -1,5 +1,9 @@
 import axios from 'axios'
 
+const config = {
+  headers: { Authorization: `Bearer` },
+}
+
 export default axios.create({
   baseURL: `http://localhost:3000/`,
 })
--- front/src/containers/Authorization/SignInContainer/hooks/useLogin.tsx
+++ front/src/containers/Authorization/SignInContainer/hooks/useLogin.tsx
@@ -2,7 +2,13 @@ import API from '../../../../api/axios'
 import { useDispatch } from 'react-redux'
 import { setCurrentUserData } from '../../../../store/reducers/currentUser'
 import { useHistory } from 'react-router-dom'
-import { TCurrentUserStoreData } from '../../../../store/reducers/currentUser/types';
+import { TCurrentUserStoreData } from '../../../../store/reducers/currentUser/types'
+import { IUser } from '../../../../shared/types/user'
+
+interface IHandleSignIn {
+  email: string
+  password: string
+}
 
 export const useLogin = () => {
   const dispatch = useDispatch()
@@ -16,25 +22,19 @@ export const useLogin = () => {
       } as TCurrentUserStoreData)
     )
 
-  const setUser = (data: any) =>
-    dispatch(
-      setCurrentUserData(data)
-    )
+  const setUser = (data: IUser) =>
+    dispatch(setCurrentUserData({ user: data } as TCurrentUserStoreData))
 
   const handleSignIn = async ({ email, password }: any) => {
     const { data } = await API.post('auth/login', { email, password })
 
     setAuthToken(data.token)
 
-    const config = {
-      headers: { Authorization: `Bearer ${data.token}` },
-    }
+    // const { data: user } = await API.get(`profile/${data.userId}`, config)
+    //
+    // setUser(user)
 
-    const { data: user } = await API.get(`profile/${data.userId}`, config)
-
-    setUser({user})
     history.push('/home')
-
   }
 
   return {
